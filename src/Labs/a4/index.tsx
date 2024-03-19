6ba028b73a77b424677de8d929c767da5ed445ac
Arnav Sawant <sawaarna9@gmail.com>
2024-03-18 20:23:25 -0400

    A4 done with lab

---
+++ src/Labs/a4/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent"
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DataStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
+
+function Assignment4() {
+  function sayHello() {
+    alert("Hello");
+  }
+  return (
+    <div>
+      <h1>Assignment 4</h1>
+      <ReduxExamples/>
+      <PassingFunctions theFunction={sayHello} />
+      <ClickEvent/>
+      <PassingDataOnEvent/>
+      <EventObject/>
+      <Counter/>
+      <BooleanStateVariables/>
+      <StringStateVariables/>
+      <DateStateVariable/>
+      <ObjectStateVariable/>
+      <ArrayStateVariable/>
+      <ParentStateComponent/>
+
+    </div>
+  );
+}
+export default Assignment4;
