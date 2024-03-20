68f17dfc414c37c608ef1da57e1f0001b3c3e34c
Arnav Sawant <sawaarna9@gmail.com>
2024-03-19 23:58:20 -0400

    should be done

--- src/Kanbas/Modules/list.tsx
+++ src/Kanbas/Modules/list.tsx
@@ -1,62 +1,59 @@
 import React, { useState } from "react";
-import "./index.css";
-import { modules } from "../Database";
-// import modules from "../Database/modules.json"
-import { FaEllipsisV, FaCheckCircle, FaPlusCircle } from "react-icons/fa";
-import { useParams } from "react-router";
-
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addModule,
+  deleteModule,
+  updateModule,
+  setModule,
+} from "./reducer";
+import { KanbasState } from "../store";
 function ModuleList() {
   const { courseId } = useParams();
-  const modulesList = modules.filter((module) => module.course === courseId);
-  console.log(courseId)
-  console.log(modulesList)
-  const [selectedModule, setSelectedModule] = useState(modulesList[0]);
+  const moduleList = useSelector((state: KanbasState) => 
+    state.modulesReducer.modules);
+  const module = useSelector((state: KanbasState) => 
+    state.modulesReducer.module);
+  const dispatch = useDispatch();
   return (
-    <>
-    <div>
-      <div className = "d-flex">
-        <button className="btn wd-modules-button-white">Collapse All</button>
-        <button className="btn wd-modules-button-white">View Progress</button>
-        <select className = "form-select wd-modules-200px-width">
-            <option>Publish All</option>
-            <option>Publish All</option>
-            <option>Publish All</option>
-        </select>
-        <button className="btn btn-secondary wd-modules-red"> <i className="fa fa-plus"></i> Module</button>
-      </div>
-      <ul className="list-group wd-modules">
-        {modulesList.map((module) => (
-          <li
-            className="list-group-item"
-            onClick={() => setSelectedModule(module)}>
-            <div>
-              <FaEllipsisV className="me-2" />
-              {module.name}
-              <span className="float-end">
-                <FaCheckCircle className="text-success" />
-                <FaPlusCircle className="ms-2" />
-                <FaEllipsisV className="ms-2" />
-              </span>
-            </div>
-            {selectedModule._id === module._id && (
-              <ul className="list-group">
-                {module.lessons?.map((lesson) => (
-                  <li className="list-group-item">
-                    <FaEllipsisV className="me-2" />
-                    {lesson.name}
-                    <span className="float-end">
-                      <FaCheckCircle className="text-success" />
-                      <FaEllipsisV className="ms-2" />
-                    </span>
-                  </li>
-                ))}
-              </ul>
-            )}
+    <ul className="list-group">
+      <li className="list-group-item">
+        <button  className="btn btn-success"
+          onClick={() => dispatch(addModule({ ...module, course: courseId }))}>
+          Add
+        </button>
+        <button className="btn btn-primary"
+          onClick={() => dispatch(updateModule(module))}>
+          Update
+        </button>
+        <input className="form-group"
+          value={module.name}
+          onChange={(e) =>
+            dispatch(setModule({ ...module, name: e.target.value }))
+          }/>
+        <textarea
+          value={module.description}
+          onChange={(e) =>
+            dispatch(setModule({ ...module, description: e.target.value }))
+          }/>
+      </li>
+      {moduleList
+        .filter((module) => module.course === courseId)
+        .map((module, index) => (
+          <li key={index} className="list-group-item">
+            <button className="btn btn-secondary"
+              onClick={() => dispatch(setModule(module))}>
+              Edit
+            </button>
+            <button className="btn btn-danger"
+              onClick={() => dispatch(deleteModule(module._id))}>
+              Delete
+            </button>
+            <h3>{module.name}</h3>
+            <p>{module.description}</p>
           </li>
         ))}
-      </ul>
-      </div>
-    </>
+    </ul>
   );
 }
 export default ModuleList;
